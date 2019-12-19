7bfd7037a269f6abf019eaf6e79de63e04702505
Daniel <p_daniel@hotmail.it>
2019-12-19 14:18:11 +0100

    fix api transactions

--- back/bankr/controllers/transactions.py
+++ back/bankr/controllers/transactions.py
@@ -7,21 +7,18 @@ from bankr.models.account import Account
 
 def get_transactions(bank=None, account=None):
 
-    retrieved_accounts = get_accounts(bank)
-
-    for retrieved_account in retrieved_accounts:
-
-        if retrieved_account.account_id == account:
-
-            transactions = Transaction.select()
-            if account is not None:
-                db_account = Account.get_or_none(account_id=account)
-                if db_account is None:
-                    raise AccountNotFoundError(account)
-
+    transactions = Transaction.select()
+    if bank is not None:
+        retrieved_accounts = get_accounts(bank)
+        if account is not None:
+            db_account = Account.get_or_none(account_id=account)
+            if db_account is None:
+                raise AccountNotFoundError(account)
+            else:
                 transactions = transactions.where(Transaction.account == db_account)
-                return transactions
-
-
         else:
-            raise AccountNotFoundError(account)
+            transactions = transactions.where(Transaction.account << retrieved_accounts)
+    else:
+        if account is not None:
+            return []
+    return transactions
